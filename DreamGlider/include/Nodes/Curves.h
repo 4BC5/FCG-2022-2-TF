6e0d58b2cc88ec25734ae889e09fa17b895b3dff
ABC <bcessart@gmail.com>
2023-03-04 22:31:29 -0300

    Atualização Curves
    funções de interpolação e definição dos pontos de controle

--- DreamGlider/include/Nodes/Curves.h
+++ DreamGlider/include/Nodes/Curves.h
@@ -16,6 +16,21 @@ class Curves : public Node3D
     public:
         Curves(std::string name);
         virtual ~Curves();
+
+        //Funções Interpolação
+        glm::vec3 inter_linear(glm::vec3 p1,glm::vec3 p2,float t);
+        glm::vec3 inter_quad(glm::vec3 p1,glm::vec3 p2,glm::vec3 p3,float t);
+        glm::vec3 inter_cub(glm::vec3 p1,glm::vec3 p2,glm::vec3 p3,glm::vec3 p4,float t);
+
+        //Pontos fixos
+        glm::vec3 A = glm::vec3(0.0f,0.0f,0.0f);
+        glm::vec3 B = glm::vec3(2.0f,1.0f,2.0f);
+        glm::vec3 C = glm::vec3(-2.0f,2.0f,-2.0f);
+        glm::vec3 D = glm::vec3(0.0f,3.0f,0.0f);
+
+        //Constrói curva
+
+
     protected:
 
     private:
--- DreamGlider/src/Nodes/Curves.cpp
+++ DreamGlider/src/Nodes/Curves.cpp
@@ -16,3 +16,19 @@ Curves::~Curves()
 {
     //dtor
 }
+
+glm::vec3 Curves::inter_linear(glm::vec3 p1,glm::vec3 p2,float t)
+{
+    return p1 + t*(p2-p1);
+}
+
+glm::vec3 Curves::inter_quad(glm::vec3 p1,glm::vec3 p2,glm::vec3 p3,float t)
+{
+    return inter_linear(p1,p2,t)+t*(inter_linear(p2,p3,t) - inter_linear(p1,p2,t));
+}
+
+glm::vec3 Curves::inter_cub(glm::vec3 p1,glm::vec3 p2,glm::vec3 p3,glm::vec3 p4,float t)
+{
+    return inter_quad(p1,p2,p3,t)+t*(inter_quad(p2,p3,p4,t)-inter_quad(p1,p2,p3,t));
+}
+
